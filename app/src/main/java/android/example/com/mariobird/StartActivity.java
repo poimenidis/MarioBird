a15af8eb36d2b24f00bf6b2fb744f2ccb71ae5ee
Kostas Poimenidis <it1530@uom.edu.gr>
2019-06-12 00:43:16 +0300

    Finish all the basics of my app

---
+++ app/src/main/java/android/example/com/mariobird/StartActivity.java
@@ -0,0 +1,41 @@
+package android.example.com.mariobird;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.FragmentActivity;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.facebook.imagepipeline.request.ImageRequest;
+import com.facebook.imagepipeline.request.ImageRequestBuilder;
+
+public class StartActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fresco.initialize(this);
+        setContentView(R.layout.activity_start);
+
+        ImageRequest imageRequest = ImageRequestBuilder.newBuilderWithResourceId(R.drawable.giphy).build();
+
+
+        SimpleDraweeView simpleDraweeView = (SimpleDraweeView) findViewById(R.id.gif);
+        simpleDraweeView.setController(
+                Fresco.newDraweeControllerBuilder()
+                        .setImageRequest(imageRequest)
+                        .setAutoPlayAnimations(true)
+                        .build());
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(StartActivity.this,MainActivity.class);
+                StartActivity.this.startActivity(mainIntent);
+                StartActivity.this.finish();
+            }
+        }, 2000);
+    }
+}
